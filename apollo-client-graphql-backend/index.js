ca4ddf650c65fa53fb4fbf1ccad740f5b750152c
MyatKaungKyaw <myatkaungg31@gmail.com>
2023-05-01 21:12:15 +0630

    Mongoose and Apollo

--- apollo-client-graphql-backend/index.js
+++ apollo-client-graphql-backend/index.js
@@ -1,7 +1,25 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const { v1: uuid } = require('uuid')
-const {GraphQLError} = require('graphql')
+const { GraphQLError } = require('graphql')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+const Person = require('./models/person')
+
+require('dotenv').config()
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
 
 let persons = [
   {
@@ -66,50 +84,34 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
-    personCount: () => persons.length,
-    allPersons: (root,args) => {
-      if(!args.phone){
-        return persons
-      }
-      const byPhone = person => args.phone === 'YES' ? person.phone : !person.phone
-      return persons.filter(byPhone)
-    },
-    findPerson: (root, args) =>
-      persons.find(p => p.name === args.name)
-  },
-  Person :{
-    address :root => {
-      return {
-        street:root.street,
-        city:root.city,
-      }
-    }  
-  },
-  Mutation: {
-    addPerson: (root, args) => {
-      if(persons.find(p => p.name === args.name)){
-        throw new GraphQLError('Name must be unique',{
-          extensions:{
-            code:'BAD_USER_INPUT',
-            invalidArgs:args.name,
-          }
-        })
-      }
-      
-      const person = { ...args, id: uuid() }
-      persons = persons.concat(person)
-      return person
-    },
-    editNumber : (root,args) => {
-      const person = persons.find(p => p.name === args.name)
-      if(!person){
-        return null
+    personCount: async () => Person.collection.countDocuments(),
+    allPersons: async (root, args) => {
+      if (!args.phone) {
+        return Person.find({})
       }
 
-      const updPerson = {...person, phone:args.phone}
-      persons = persons.map(p => p.name === args.name ? updPerson : p)
-      return updPerson
-    }
+      return Person.find({ phone: { $exists: args.phone === 'YES' } })
+    },
+    findPerson: async (root, args) => Person.findOne({ name: args.name }),
+  },
+  Person: {
+    address: (root) => {
+      return {
+        street: root.street,
+        city: root.city,
+      }
+    },
+  },
+  Mutation: {
+    addPerson: async (root, args) => {
+      const person = new Person({ ...args })
+      return person.save()
+    },
+    editNumber: async (root, args) => {
+      const person = await Person.findOne({ name: args.name })
+      person.phone = args.phone
+      return person.save()
+    },
   },
 }
 
